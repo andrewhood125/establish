1bee6ed41000ecfb365274d4162a92be8aa3d1c5
Andrew Hood <andrewhood125@gmail.com>
2015-02-27 19:15:33 +0000

    What is tee? No one really knows.echo "deb http://http.debian.net/debian wheezy-backports main"

--- debian/nodejs.sh
+++ debian/nodejs.sh
@@ -1,7 +1,7 @@
 echo -e "\t${BASH_SOURCE}"
 
 _nodejs_up() {
-  echo "deb http://http.debian.net/debian wheezy-backports main" > /etc/apt/sources.list.d/establish.list
+  eval "echo 'deb http://http.debian.net/debian wheezy-backports main' | sudo tee -a /etc/apt/sources.list.d/establish.list"
   eval "sudo apt-get update"
   eval "sudo apt-get -t wheezy-backports install nodejs"
   eval "sudo ln -s /usr/bin/nodejs /usr/bin/node"
