80d80f932d6f13b8748f4cbec5dfb5940ceb239b
Andrew Hood <andrewhood125@gmail.com>
2015-03-03 14:09:58 -0600

    Fix non matching "

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/andrewhood125/establish.git ~/.establish
 echo -e "Add establish to your path:\n"
-echo -e "\t export PATH="$PATH:$HOME/.establish/bin\n"
+echo -e '\t export PATH="$PATH:$HOME/.establish/bin\n"'
