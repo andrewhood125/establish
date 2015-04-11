f75fe95dbd8e510a5561f7bd08998a4b49fb5704
Andrew Hood <andrewhood125@gmail.com>
2015-04-11 17:48:05 -0500

    Can not pipe stdout to null because maria prompts for root password

--- debian/mariadb.sh
+++ debian/mariadb.sh
@@ -9,7 +9,7 @@ _mariadb_up() {
   sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
   sudo add-apt-repository 'deb http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.0/debian wheezy main'
   sudo apt-get -qq update
-  sudo apt-get -y install mariadb-server > /dev/null
+  sudo apt-get -qq install mariadb-server
 }
 
 # Upgrade mariadb
