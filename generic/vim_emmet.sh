73614539986a247b184ecb6fb10a2ddf69a05d11
Andrew Hood <andrewhood125@gmail.com>
2015-04-25 17:07:11 -0500

    Add vim-emmet

---
+++ generic/vim_emmet.sh
@@ -0,0 +1,24 @@
+# vim-emmet dependencies
+vim_emmet_deps=(pathogen)
+
+vim_emmet_dir="${HOME}/.vim/bundle/vim-emmet"
+
+# Is vim-emmet installed?
+_vim_emmet_installed() {
+  [ -d "$vim_emmet_dir" ]
+}
+
+# Install vim-emmet
+_vim_emmet_up() {
+  git clone --quiet https://github.com/mattn/vim-emmet $vim_emmet_dir
+}
+
+# Upgrade vim-emmet
+_vim_emmet_upgrade() {
+  cd $vim_emmet_dir && git pull --quiet > /dev/null
+}
+
+# Remove vim-emmet
+_vim_emmet_down() {
+  rm -rf $vim_emmet_dir
+}
